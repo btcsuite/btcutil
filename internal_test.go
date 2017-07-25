13a7b1f763243f6d78318bc29fe2bf071e3365cb
Johan T. Halseth <johanth@gmail.com>
2017-07-25 07:47:41 +0200

    bech32 encoded segwit addresses.
    This commit adds support for bech32 encoded segwit
    addresses, namely AddressWitnessPubKeyHash and
    AddressWitnessScriptHash. These are both specified
    in BIP 173.

--- internal_test.go
+++ internal_test.go
@@ -14,6 +14,7 @@ package btcutil
 import (
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcutil/base58"
+	"github.com/btcsuite/btcutil/bech32"
 	"golang.org/x/crypto/ripemd160"
 )
 
@@ -52,6 +53,30 @@ func TstAddressScriptHash(hash [ripemd160.Size]byte,
 	}
 }
 
+// TstAddressWitnessPubKeyHash creates an AddressWitnessPubKeyHash, initiating
+// the fields as given.
+func TstAddressWitnessPubKeyHash(version byte, program [20]byte,
+	hrp string) *AddressWitnessPubKeyHash {
+
+	return &AddressWitnessPubKeyHash{
+		hrp:            hrp,
+		witnessVersion: version,
+		witnessProgram: program,
+	}
+}
+
+// TstAddressWitnessScriptHash creates an AddressWitnessScriptHash, initiating
+// the fields as given.
+func TstAddressWitnessScriptHash(version byte, program [32]byte,
+	hrp string) *AddressWitnessScriptHash {
+
+	return &AddressWitnessScriptHash{
+		hrp:            hrp,
+		witnessVersion: version,
+		witnessProgram: program,
+	}
+}
+
 // TstAddressPubKey makes an AddressPubKey, setting the unexported fields with
 // the parameters.
 func TstAddressPubKey(serializedPubKey []byte, pubKeyFormat PubKeyFormat,
@@ -70,4 +95,20 @@ func TstAddressPubKey(serializedPubKey []byte, pubKeyFormat PubKeyFormat,
 func TstAddressSAddr(addr string) []byte {
 	decoded := base58.Decode(addr)
 	return decoded[1 : 1+ripemd160.Size]
+}
+
+// TstAddressSegwitSAddr returns the expected witness program bytes for
+// bech32 encoded P2WPKH and P2WSH bitcoin addresses.
+func TstAddressSegwitSAddr(addr string) []byte {
+	_, data, err := bech32.Decode(addr)
+	if err != nil {
+		return []byte{}
+	}
+
+	// First byte is version, rest is base 32 encoded data.
+	data, err = bech32.ConvertBits(data[1:], 5, 8, false)
+	if err != nil {
+		return []byte{}
+	}
+	return data
 }

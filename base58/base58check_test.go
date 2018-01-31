4443e04cb81212c3c560250f0d16b022ad47ce67
Steven Roose <stevenroose@gmail.com>
2018-01-31 18:01:49 +0100

    Fix error in base58check test

--- base58/base58check_test.go
+++ base58/base58check_test.go
@@ -56,7 +56,7 @@ func TestBase58Check(t *testing.T) {
 	// bytes are missing).
 	testString := ""
 	for len := 0; len < 4; len++ {
-		// make a string of length `len`
+		testString = testString + "x"
 		_, _, err = base58.CheckDecode(testString)
 		if err != base58.ErrInvalidFormat {
 			t.Error("Checkdecode test failed, expected ErrInvalidFormat")

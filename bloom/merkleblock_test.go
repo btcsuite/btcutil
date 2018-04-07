4bfe47acc765e8ddc0de33b06ac8a1e1724e72c6
amanusk <alex.manuskin@gmail.com>
2018-04-07 16:10:27 +0300

    bloom: Add encoding type to filter tests
    The current version of btcd requires 3 parameters for the test to work,
    this add wire.BaseEncoding parameter

--- bloom/merkleblock_test.go
+++ bloom/merkleblock_test.go
@@ -60,7 +60,7 @@ func TestMerkleBlock3(t *testing.T) {
 	}
 
 	got := bytes.NewBuffer(nil)
-	err = mBlock.BtcEncode(got, wire.ProtocolVersion)
+	err = mBlock.BtcEncode(got, wire.ProtocolVersion, wire.BaseEncoding)
 	if err != nil {
 		t.Errorf("TestMerkleBlock3 BtcEncode failed: %v", err)
 		return

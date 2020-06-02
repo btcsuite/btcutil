cfe25815fe0108d0670e2ddf2fc705a0ab2bbe40
Peter Rabbitson <ribasushi@leporine.io>
2020-06-02 19:58:18 +0200

    Significant time/space reduction in address encoding/decoding
    === Before
    $ go test ./base58/ -bench=. -benchmem
    goos: darwin
    goarch: amd64
    pkg: github.com/btcsuite/btcutil/base58
    BenchmarkBase58EncodeAddr-8   	  195880	      5864 ns/op	   5.80 MB/s	     648 B/op	      51 allocs/op
    BenchmarkBase58DecodeAddr-8   	  290460	      4115 ns/op	  11.42 MB/s	     264 B/op	       8 allocs/op
    BenchmarkBase58Encode5k-8     	      18	  63641483 ns/op	   0.08 MB/s	   82440 B/op	    6725 allocs/op
    BenchmarkBase58Decode5k-8     	     226	   5334819 ns/op	   1.28 MB/s	 1024873 B/op	     380 allocs/op
    
    === After
    $ go test ./base58/ -bench=. -benchmem
    goos: darwin
    goarch: amd64
    pkg: github.com/btcsuite/btcutil/base58
    BenchmarkBase58EncodeAddr-8   	  861850	      1358 ns/op	  25.03 MB/s	     144 B/op	       2 allocs/op
    BenchmarkBase58DecodeAddr-8   	 1322410	       903 ns/op	  52.07 MB/s	     400 B/op	       3 allocs/op
    BenchmarkBase58Encode5k-8     	      43	  27442568 ns/op	   0.18 MB/s	   21248 B/op	       2 allocs/op
    BenchmarkBase58Decode5k-8     	      93	  12375857 ns/op	   0.55 MB/s	   57344 B/op	       3 allocs/op

--- base58/base58.go
+++ base58/base58.go
@@ -5,71 +5,12 @@
 package base58
 
 import (
-	"math/big"
+	"github.com/mr-tron/base58/base58"
 )
 
-//go:generate go run genalphabet.go
+var Encode = base58.Encode
 
-var bigRadix = big.NewInt(58)
-var bigZero = big.NewInt(0)
-
-// Decode decodes a modified base58 string to a byte slice.
-func Decode(b string) []byte {
-	answer := big.NewInt(0)
-	j := big.NewInt(1)
-
-	scratch := new(big.Int)
-	for i := len(b) - 1; i >= 0; i-- {
-		tmp := b58[b[i]]
-		if tmp == 255 {
-			return []byte("")
-		}
-		scratch.SetInt64(int64(tmp))
-		scratch.Mul(j, scratch)
-		answer.Add(answer, scratch)
-		j.Mul(j, bigRadix)
-	}
-
-	tmpval := answer.Bytes()
-
-	var numZeros int
-	for numZeros = 0; numZeros < len(b); numZeros++ {
-		if b[numZeros] != alphabetIdx0 {
-			break
-		}
-	}
-	flen := numZeros + len(tmpval)
-	val := make([]byte, flen)
-	copy(val[numZeros:], tmpval)
-
-	return val
-}
-
-// Encode encodes a byte slice to a modified base58 string.
-func Encode(b []byte) string {
-	x := new(big.Int)
-	x.SetBytes(b)
-
-	answer := make([]byte, 0, len(b)*136/100)
-	for x.Cmp(bigZero) > 0 {
-		mod := new(big.Int)
-		x.DivMod(x, bigRadix, mod)
-		answer = append(answer, alphabet[mod.Int64()])
-	}
-
-	// leading zero bytes
-	for _, i := range b {
-		if i != 0 {
-			break
-		}
-		answer = append(answer, alphabetIdx0)
-	}
-
-	// reverse
-	alen := len(answer)
-	for i := 0; i < alen/2; i++ {
-		answer[i], answer[alen-1-i] = answer[alen-1-i], answer[i]
-	}
-
-	return string(answer)
+func Decode(in string) (out []byte) {
+	out, _ = base58.Decode(in)
+	return
 }

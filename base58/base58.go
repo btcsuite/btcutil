9254c287db4f1a8034772f95cf6ac19331f34b6a
Egon Elbre <egonelbre@gmail.com>
2020-09-10 17:54:35 +0300

    base58: optimize Encode
    Before:
        BenchmarkBase58Encode_5K-32     46      23934763 ns/op  0.21 MB/s
        BenchmarkBase58Encode_100K-32    1    9351948600 ns/op  0.01 MB/s
    
    After:
        BenchmarkBase58Encode_5K-32    501       2419129 ns/op  2.07 MB/s
        BenchmarkBase58Encode_100K-32    2     923507950 ns/op  0.11 MB/s

--- base58/base58.go
+++ base58/base58.go
@@ -11,6 +11,7 @@ import (
 //go:generate go run genalphabet.go
 
 var bigRadix = big.NewInt(58)
+var bigRadix10 = big.NewInt(58 * 58 * 58 * 58 * 58 * 58 * 58 * 58 * 58 * 58) // 58^10
 var bigZero = big.NewInt(0)
 
 // Decode decodes a modified base58 string to a byte slice.
@@ -51,10 +52,32 @@ func Encode(b []byte) string {
 	x.SetBytes(b)
 
 	answer := make([]byte, 0, len(b)*136/100)
+	mod := new(big.Int)
 	for x.Cmp(bigZero) > 0 {
-		mod := new(big.Int)
-		x.DivMod(x, bigRadix, mod)
-		answer = append(answer, alphabet[mod.Int64()])
+		// Calculating with big.Int is slow for each iteration.
+		//    x, mod = x / 58, x % 58
+		//
+		// Instead we can try to do as much calculations on int64.
+		//    x, mod = x / 58^10, x % 58^10
+		//
+		// Which will give us mod, which is 10 digit base58 number.
+		// We'll loop that 10 times to convert to the answer.
+
+		x.DivMod(x, bigRadix10, mod)
+		if x.Cmp(bigZero) == 0 {
+			// When x = 0, we need to ensure we don't add any extra zeros.
+			m := mod.Int64()
+			for m > 0 {
+				answer = append(answer, alphabet[m%58])
+				m /= 58
+			}
+		} else {
+			m := mod.Int64()
+			for i := 0; i < 10; i++ {
+				answer = append(answer, alphabet[m%58])
+				m /= 58
+			}
+		}
 	}
 
 	// leading zero bytes

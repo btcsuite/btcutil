btcsuite/btcutil

commit 24e673ae72b5b4fad36808ae685489b2c26468e2

FIX: integers incorrectly encoded as a rune with append. Go no longer allows it.

address.go
@@ -23,7 +23,7 @@ import (
 type UnsupportedWitnessVerError byte
 
 func (e UnsupportedWitnessVerError) Error() string {
-	return "unsupported witness version: " + string(e)
+	return fmt.Sprintf("unsupported witness version: %#x", e)
 }
 
 // UnsupportedWitnessProgLenError describes an error where a segwit address
@@ -31,7 +31,7 @@ func (e UnsupportedWitnessVerError) Error() string {
 type UnsupportedWitnessProgLenError int
 
 func (e UnsupportedWitnessProgLenError) Error() string {
-	return "unsupported witness program length: " + string(e)
+	return fmt.Sprintf("unsupported witness program length: %d", e)
 }
 
 var (

bech32/bech32_test.go
@@ -23,7 +23,7 @@ func TestBech32(t *testing.T) {
 		{"split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w", true},
 		{"split1checkupstagehandshakeupstreamerranterredcaperred2y9e2w", false},                                // invalid checksum
 		{"s lit1checkupstagehandshakeupstreamerranterredcaperredp8hs2p", false},                                // invalid character (space) in hrp
-		{"spl" + string(127) + "t1checkupstagehandshakeupstreamerranterredcaperred2y9e3w", false},              // invalid character (DEL) in hrp
+		{"spl\x7Ft1checkupstagehandshakeupstreamerranterredcaperred2y9e3w", false},              // invalid character (DEL) in hrp
 		{"split1cheo2y9e2w", false},                                                                            // invalid character (o) in data part
 		{"split1a2y9w", false},                                                                                 // too short data part
 		{"1checkupstagehandshakeupstreamerranterredcaperred2y9e3w", false},                                     // empty hrp

commit 4ebb6a99602f113ac6bc30982310bfd9d2662b26

address: fix stack overflow bug when printing error
Commit 24e673ae introduced a bug that cause the Error() function to
call itself recursively forever, causing a stack overflow. We explicitly
cast the error to its base type to avoid the recursion and add a small
test case that would've triggered the bug before.

address.go
@@ -23,7 +23,7 @@ import (
 type UnsupportedWitnessVerError byte
 
 func (e UnsupportedWitnessVerError) Error() string {
-	return fmt.Sprintf("unsupported witness version: %#x", e)
+	return fmt.Sprintf("unsupported witness version: %#x", byte(e))
 }
 
 // UnsupportedWitnessProgLenError describes an error where a segwit address
@@ -31,7 +31,7 @@ func (e UnsupportedWitnessVerError) Error() string {
 type UnsupportedWitnessProgLenError int
 
 func (e UnsupportedWitnessProgLenError) Error() string {
-	return fmt.Sprintf("unsupported witness program length: %d", e)
+	return fmt.Sprintf("unsupported witness program length: %d", int(e))
 }
 
 var (

address_test.go
@@ -859,6 +859,14 @@ func TestAddresses(t *testing.T) {
 					test.name)
 				return
 			}
+		} else {
+			// If there is an error, make sure we can print it
+			// correctly.
+			errStr := err.Error()
+			if errStr == "" {
+				t.Errorf("%v: error was non-nil but message is"+
+					"empty: %v", test.name, err)
+			}
 		}
 
 		if !test.valid {
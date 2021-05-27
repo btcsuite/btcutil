4ebb6a99602f113ac6bc30982310bfd9d2662b26
Oliver Gugger <gugger@gmail.com>
2021-05-27 10:08:03 +0200

    address: fix stack overflow bug when printing error
    Commit 24e673ae introduced a bug that cause the Error() function to
    call itself recursively forever, causing a stack overflow. We explicitly
    cast the error to its base type to avoid the recursion and add a small
    test case that would've triggered the bug before.

--- address.go
+++ address.go
@@ -23,7 +23,7 @@ import (
 type UnsupportedWitnessVerError byte
 
 func (e UnsupportedWitnessVerError) Error() string {
-	return fmt.Sprintf("unsupported witness version: %#x", e)
+	return fmt.Sprintf("unsupported witness version: %#x", byte(e))
 }
 
 // UnsupportedWitnessProgLenError describes an error where a segwit address
@@ -31,7 +31,7 @@ func (e UnsupportedWitnessVerError) Error() string {
 type UnsupportedWitnessProgLenError int
 
 func (e UnsupportedWitnessProgLenError) Error() string {
-	return fmt.Sprintf("unsupported witness program length: %d", e)
+	return fmt.Sprintf("unsupported witness program length: %d", int(e))
 }
 
 var (
